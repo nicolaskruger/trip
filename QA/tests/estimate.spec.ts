ec472e8e64a7cb46890473ad4a84af1ab1b93d75
nkruger <nicolas.nkruger@gmail.com>
2024-11-23 10:26:44 -0300

    feat: :sparkles: validate estimate

--- QA/tests/estimate.spec.ts
+++ QA/tests/estimate.spec.ts
@@ -13,11 +13,57 @@ type Request = {
   destination?: string;
 };
 
+type Response = {
+  origin: {
+    latitude: number;
+    longitude: number;
+  };
+  destination: {
+    latitude: number;
+    longitude: number;
+  };
+  distance: number;
+  duration: string;
+  options: [
+    {
+      id: number;
+      name: string;
+      description: string;
+      vehicle: string;
+      review: {
+        rating: number;
+        comment: string;
+      };
+      value: number;
+    }
+  ];
+  routeResponse: object;
+};
+
+const objComp = {
+  origin: {
+    latitude: true,
+    longitude: true,
+  },
+  destination: {
+    latitude: true,
+    longitude: true,
+  },
+  distance: true,
+  duration: true,
+};
 const getRequest = (): Request => ({
   customer_id: "12345",
   origin: "New York",
   destination: "Los Angeles",
 });
+
+const deepValidationTruthy = (data: any, ref: any) => {
+  Object.keys(ref).forEach((key) => {
+    if (typeof ref[key] === "object") deepValidationTruthy(data[key], ref[key]);
+    else expect(data[key]).toBeTruthy();
+  });
+};
 
 test("empty origin", async ({ request }) => {
   const req = getRequest();
@@ -26,4 +72,42 @@ test("empty origin", async ({ request }) => {
   expect(res.status()).toBe(400);
   const json: ErrorRes = await res.json();
   expect(json.error_code).toBe("INVALID_DATA");
+});
+
+test("empty destination", async ({ request }) => {
+  const req = getRequest();
+  delete req.destination;
+  const res = await request.post(URL, { data: req });
+  expect(res.status()).toBe(400);
+  const json: ErrorRes = await res.json();
+  expect(json.error_code).toBe("INVALID_DATA");
+});
+
+test("empty customer id", async ({ request }) => {
+  const req = getRequest();
+  delete req.customer_id;
+  const res = await request.post(URL, { data: req });
+  expect(res.status()).toBe(400);
+  const json: ErrorRes = await res.json();
+  expect(json.error_code).toBe("INVALID_DATA");
+});
+
+test("empty same origin", async ({ request }) => {
+  const req = getRequest();
+  req.origin = req.destination;
+  const res = await request.post(URL, { data: req });
+  expect(res.status()).toBe(400);
+  const json: ErrorRes = await res.json();
+  expect(json.error_code).toBe("INVALID_DATA");
+});
+
+test("susses", async ({ request }) => {
+  const req = getRequest();
+  const res = await request.post(URL, { data: req });
+  expect(res.status()).toBe(200);
+  const json: Response = await res.json();
+
+  deepValidationTruthy(json, objComp);
+  expect(json.options.length).toBe(3);
+  expect(json.routeResponse).toBeTruthy();
 });
