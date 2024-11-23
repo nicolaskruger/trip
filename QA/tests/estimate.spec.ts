85e81b87f99bdbb16f2fdf6e9f8c2124366cb902
nkruger <nicolas.nkruger@gmail.com>
2024-11-23 10:10:27 -0300

    feat: :sparkles: first test

--- QA/tests/estimate.spec.ts
+++ QA/tests/estimate.spec.ts
@@ -1,1 +1,29 @@
-const URL = "http";
+const URL = "http://localhost:8080/ride/estimate";
+
+import { expect, test } from "@playwright/test";
+
+type ErrorRes = {
+  error_code: "INVALID_DATA";
+  error_description: string;
+};
+
+type Request = {
+  customer_id?: string;
+  origin?: string;
+  destination?: string;
+};
+
+const getRequest = (): Request => ({
+  customer_id: "12345",
+  origin: "New York",
+  destination: "Los Angeles",
+});
+
+test("empty origin", async ({ request }) => {
+  const req = getRequest();
+  delete req.origin;
+  const res = await request.post(URL, { data: req });
+  expect(res.status()).toBe(400);
+  const json: ErrorRes = await res.json();
+  expect(json.error_code).toBe("INVALID_DATA");
+});
