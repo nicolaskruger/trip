a8173883c30597723a50f6ab70f52a3cc223c3f3
nkruger <nicolas.nkruger@gmail.com>
2024-11-26 14:49:52 -0300

    test: :white_check_mark: confirm

--- backend/src/service/ride.service.spec.ts
+++ backend/src/service/ride.service.spec.ts
@@ -2,6 +2,7 @@ import { MapRoute, MapsRepository } from 'src/repository/maps.repository';
 import { Costumer, RideService } from './ride.service';
 import { MongoTripRepository } from 'src/repository/mongo.trip.repository';
 import { Driver } from 'src/repository/schema/driver.schema';
+import { Document } from 'mongoose';
 
 const getDriver = (): Driver =>
   ({
@@ -62,17 +63,47 @@ const getCostumer = (): Costumer => ({
   destination: 'Novo Hamburgo',
 });
 
+type Order = {
+  customer_id: string;
+  origin: string;
+  destination: string;
+  distance: number;
+  duration: string;
+  driver: { id: number; name: string };
+  value: number;
+};
+
+const getOrder = (): Order => ({
+  customer_id: '12345-abcde',
+  origin: 'New York, NY',
+  destination: 'Los Angeles, CA',
+  distance: 4500.5,
+  duration: '40h',
+  driver: {
+    id: 1,
+    name: 'John Doe',
+  },
+  value: 1250.75,
+});
+
 describe('ride test', () => {
   const mapRepository = jest.mocked({} as MapsRepository);
   const mongoTripRepository = jest.mocked({} as MongoTripRepository);
-
   mapRepository.calc = jest.fn();
-  mapRepository.calc.mockResolvedValue(getMapRoute());
-
   mongoTripRepository.pickDriver = jest.fn();
-  mongoTripRepository.pickDriver.mockResolvedValue([getDriver()]);
-
+  mongoTripRepository.findDriver = jest.fn();
   mongoTripRepository.updatePreOrder = jest.fn();
+  mongoTripRepository.saveOrder = jest.fn();
+
+  beforeEach(() => {
+    mapRepository.calc.mockResolvedValue(getMapRoute());
+    mongoTripRepository.pickDriver.mockResolvedValue([getDriver()]);
+    mongoTripRepository.findDriver.mockResolvedValue(getDriver() as any);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   const rideService: RideService = new RideService(
     mapRepository,
@@ -140,6 +171,80 @@ describe('ride test', () => {
         expect(estimate.duration).toBe('5000s');
         expect(estimate.routeResponse).toBeTruthy();
       } catch (error) {
+        expect(true).toBeFalsy();
+        expect(error).toBeFalsy();
+      }
+    });
+  });
+
+  describe('confirm', () => {
+    it('empty origin', async () => {
+      try {
+        const order = getOrder();
+        delete order.origin;
+        await rideService.confirm(order);
+        expect(true).toBeFalsy();
+      } catch (error) {
+        expect(error.code).toBe(400);
+        expect(error.error_code).toBe('INVALID_DATA');
+      }
+    });
+
+    it('empty destination', async () => {
+      try {
+        const order = getOrder();
+        delete order.destination;
+        await rideService.confirm(order);
+        expect(true).toBeFalsy();
+      } catch (error) {
+        expect(error.code).toBe(400);
+        expect(error.error_code).toBe('INVALID_DATA');
+      }
+    });
+
+    it('empty costumer', async () => {
+      try {
+        const order = getOrder();
+        delete order.customer_id;
+        await rideService.confirm(order);
+        expect(true).toBeFalsy();
+      } catch (error) {
+        expect(error.code).toBe(400);
+        expect(error.error_code).toBe('INVALID_DATA');
+      }
+    });
+
+    it('invalid driver', async () => {
+      try {
+        const order = getOrder();
+        mongoTripRepository.findDriver.mockResolvedValue(null);
+        await rideService.confirm(order);
+        expect(true).toBeFalsy();
+      } catch (error) {
+        expect(error.code).toBe(404);
+        expect(error.error_code).toBe('DRIVER_NOT_FOUND');
+      }
+    });
+
+    it('invalid meters', async () => {
+      try {
+        const order = getOrder();
+        order.distance = 0;
+        await rideService.confirm(order);
+        expect(true).toBeFalsy();
+      } catch (error) {
+        expect(error.code).toBe(406);
+        expect(error.error_code).toBe('INVALID_DISTANCE');
+      }
+    });
+    it('success', async () => {
+      try {
+        const order = getOrder();
+        order.distance = 100000000;
+        await rideService.confirm(order);
+        expect(true).toBeTruthy();
+      } catch (error) {
+        expect(error).toBeFalsy();
         expect(true).toBeFalsy();
       }
     });
